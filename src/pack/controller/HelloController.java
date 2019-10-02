70903719a03ac5989ed2810162e7225246867401
shyong0427 <shyong0427@daum.net>
2019-10-02 11:49:05 +0900

    2019-10-02 Spring_Web_Summary_Annotation Update Ver.3_요청이름만 출력한다.

--- src/pack/controller/HelloController.java
+++ src/pack/controller/HelloController.java
@@ -1,9 +1,13 @@
 package pack.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,6 +22,7 @@ public class HelloController {
 	@Autowired // Sector Insection
 	private HelloModel helloModel;
 
+	/*
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView summaryGET() {
 		String result = helloModel.getGreeting();
@@ -26,5 +31,29 @@ public class HelloController {
 		modelAndView.setViewName("hello");
 		modelAndView.addObject("result", result);
 		return modelAndView;
+	}
+	*/
+	
+	/*
+	@RequestMapping(method = RequestMethod.GET)
+	public Map<String, Object> summaryGET() {
+		String result = helloModel.getGreeting();
+		
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("result", result);
+		
+		return map;
+		/*
+		 * 웹에서 요청이 hello.do로 왔기 때문에 hello인 파일만 찾아 출력한다.
+		 * 
+	}*/
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public Model summaryGET(Model model) {
+		String result = helloModel.getGreeting();
+		
+		model.addAttribute("result", result);
+		
+		return model;
 	}
 }
